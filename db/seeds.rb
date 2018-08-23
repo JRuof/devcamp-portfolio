1a4bb999d37ab2ce8c93fa747e2c0462e77c4b18
Johannes Ruof <johannesruof@Johanness-MBP.fritz.box>
2018-08-23 17:32:08 +0200

    New Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
-		body: "My Blog Body #{blog}"
+		body: "Spicy jalapeno bacon ipsum dolor amet qui shankle consectetur tenderloin, ground round excepteur andouille salami. Consequat do leberkas, minim venison bacon deserunt. Bresaola venison sunt swine in burgdoggen doner in adipisicing quis ea ut. Short loin irure doner frankfurter flank ex. Elit in beef ribs picanha deserunt filet mignon. Tenderloin sausage cillum, capicola prosciutto non ipsum bacon doner est fatback nisi reprehenderit enim tongue."
 	)
 end
 
@@ -20,7 +20,7 @@ puts "Skills created"
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
 		subtitle: "This is so great!",
-		body: "Lorem Ipsum",
+		body: "Brisket leberkas pork loin pariatur kevin pork belly. Ground round capicola picanha, ball tip kevin biltong non enim shank incididunt porchetta tempor. Pork nisi tempor incididunt chuck, ipsum capicola laboris buffalo ex brisket strip steak meatball cupidatat. Irure ut kevin sausage. Capicola ut ut elit quis.",
 		main_image: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/300x150"
 	)
