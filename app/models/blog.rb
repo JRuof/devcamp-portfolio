b0da87a20631b08d3afb99cfdf144bdfa987ed6f
Johannes Ruof <johannesruof@Johanness-MacBook-Pro.local>
2018-08-23 20:32:42 +0200

    validating presence for blog posts, skills and portfolios

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1 }
 	extend FriendlyId
 	friendly_id :title, use: :slugged
+
+	# No blog posts can be create without the parameters specified here
+	validates_presence_of :title, :body
 end
